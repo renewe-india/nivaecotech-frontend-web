1afd30d1c8316a5520e829d45c837e04e3445607
Vansh-renewe <vansh.kapoor@renewe.in>
2024-07-07 16:49:17 +0530

    Major update

---
+++ src/app/(app)/about-us/page.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <div
+        className="container-fluid  mb-5 py-5"
+        style={{
+          background:
+            "linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)), url('/display-flex.jpg') center center no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="container">
+          <hr className="w-full h-3 mx-auto bg-white border-0 rounded dark:bg-gray-700"></hr>
+          <h1 className="display-3 text-center text-white mb-3 animated slideInDown">
+            About Us
+          </h1>
+          <hr className="w-full h-3 mx-auto bg-white border-0 rounded dark:bg-gray-700"></hr>
+        </div>
+      </div>
+      <div className="container-xxl py-5">
+        <div className="container">
+          <div className="row g-4">
+            <div
+              className="col-md-6 animate-fade-in-up"
+              style={{ animationDelay: "0.1s", visibility: "visible" }}
+            >
+              <h6 className="text-[#ffa247] text-uppercase">Get In Touch</h6>
+              <h3 className="font-bold">NIVA ECOTECH PVT. LTD.</h3>
+              <div className="mb-2 d-flex align-items-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Tellus pellentesque eu tincidunt tortor aliquam nulla. In dictum
+                non consectetur a. Sed viverra ipsum nunc aliquet bibendum enim
+                facilisis gravida. Ultrices tincidunt arcu non sodales neque. Eu
+                sem integer vitae justo eget magna fermentum iaculis.
+                Scelerisque felis imperdiet proin fermentum leo. Risus sed
+                vulputate odio ut enim blandit volutpat. In hendrerit gravida
+                rutrum quisque non tellus orci ac auctor. Urna nec tincidunt
+                praesent semper feugiat nibh sed. Faucibus interdum posuere
+                lorem ipsum. Et molestie ac feugiat sed lectus. Aliquam nulla
+                facilisi cras fermentum odio eu. Vitae sapien pellentesque
+                habitant morbi tristique senectus. Aliquam nulla facilisi cras
+                fermentum odio eu. Integer malesuada nunc vel risus commodo
+                viverra.
+              </div>
+            </div>
+            <div
+              className="col-md-6 animate-fade-in-up"
+              style={{ animationDelay: "0.1s", visibility: "visible" }}
+            >
+              <div className="relative h-64 overflow-hidden sm:h-80 lg:h-full">
+                <Image
+                  src="/card/maintenance2.jpg"
+                  alt=""
+                  fill
+                  className="rounded-xl"
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default page;
---
+++ src/app/(app)/download-center/page.tsx
@@ -0,0 +1,89 @@
+"use client";
+import React from "react";
+
+type File = {
+  url: string;
+  name: string;
+};
+
+const Branding: File[] = [
+  { url: "/brain.jpg", name: "Logo JPG" },
+  { url: "/path/to/file2.pdf", name: "Logo CDR" },
+  { url: "/path/to/file3.pdf", name: "Logo Short" },
+];
+const Company: File[] = [
+  { url: "/brain.jpg", name: "Profile" },
+  { url: "/path/to/file2.pdf", name: "Investor T&C" },
+  { url: "/path/to/file3.pdf", name: "Client T&C" },
+];
+
+const DownloadSection: React.FC = () => {
+  const handleDownload = (fileUrl: string, fileName: string) => {
+    const link = document.createElement("a");
+    link.href = fileUrl;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <>
+      <div
+        className="container-fluid  mb-5 py-5"
+        style={{
+          background:
+            "linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)), url('/stars.jpg') center center no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="container">
+          <hr className="w-full h-3 mx-auto bg-white border-0 rounded dark:bg-gray-700"></hr>
+          <div className="flex flex-row align-items-center  gap-10">
+            <h1 className="display-5 text-white animated slideInDown">
+              Download center
+            </h1>
+          </div>
+          <hr className="w-full h-3 mx-auto bg-white border-0 rounded dark:bg-gray-700"></hr>
+        </div>
+      </div>
+      <div className="container-xxl py-5">
+        <div className="container">
+          <h6 className="text-[#FF4917] text-left text-uppercase font-bold">
+            Access everything you need a hub of hassle-free possibilities.
+          </h6>
+          <h1 className="font-semibold text-left my-3">Branding</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {Branding.map((file, index) => (
+              <div key={index} className="p-4 border rounded-xl">
+                <h3>{file.name}</h3>
+                <button
+                  className="mt-2 px-4 py-2 bg-[#EE7532] text-white rounded"
+                  onClick={() => handleDownload(file.url, file.name)}
+                >
+                  Download
+                </button>
+              </div>
+            ))}
+          </div>
+          <h1 className="font-semibold text-left my-3">Company</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {Company.map((file, index) => (
+              <div key={index} className="p-4 border rounded-xl">
+                <h3>{file.name}</h3>
+                <button
+                  className="mt-2 px-4 py-2 bg-[#EE7532] text-white rounded"
+                  onClick={() => handleDownload(file.url, file.name)}
+                >
+                  Download
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DownloadSection;
